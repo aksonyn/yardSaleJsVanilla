09d29213fd76e26613ad737793ee56948e5f0a30
aksonyn <aksonynadames@gmail.com>
2022-07-20 17:32:22 -0400

    se fuisiono menu desktop y menu mobile con navbar

--- main.js
+++ main.js
@@ -1,11 +1,21 @@
 //toggle menu Desktop
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const burgerMenuBtn = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+burgerMenuBtn.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu(){
 
     desktopMenu.classList.toggle('inactive');
 
 };
+
+function toggleMobileMenu(){
+
+    mobileMenu.classList.toggle('inactive');
+
+};
+
